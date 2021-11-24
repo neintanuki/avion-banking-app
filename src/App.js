2d678a5081d1432962a4e513e764ae8b697b0d0c
= <=>
2021-11-24 19:52:05 +0800

    added sign-in feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import Route from './router/Route';
 import Link from './router/Link';
 
 import SignIn from './pages/SignIn';
+import { useState } from 'react';
+
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   return (
     <>
-    <Route path="/signin"><SignIn /></Route>
+    <Route path="/signin"><SignIn setIsSignedIn={setIsSignedIn} /></Route>
     </>
   );
 }
